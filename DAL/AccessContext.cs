586452516776297748a68a5702eaac25c2a49bbf
Gr_Em <ragtime@ukr.net>
2018-05-28 15:26:14 +0300

    AccessContext was ceated

---
+++ DAL/AccessContext.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class AccessContext: DbContext
+    {
+        public AccessContext():base ("DbConnection")
+        {
+        }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+    }
+}
